ffae549dd5e6c4c3e43623f85610d6bd0009b802
gaurav-geeky <gauravmpreat@gmail.com>
2026-02-01 17:05:40 +0530

    live

--- back/index.js
+++ back/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-            "https://aganitha-paste-bin-assessemnt.vercel.app",
+            "pastebin-woad.vercel.app",
             "http://localhost:5173",
         ],
     })
