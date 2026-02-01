faab75fd17b1ab030c0e2be9e594fc0ce5acd733
gaurav-geeky <gauravmpreat@gmail.com>
2026-02-01 17:32:34 +0530

    cors

--- back/index.js
+++ back/index.js
@@ -10,29 +10,14 @@ const app = express();
 
 // app.use(cors());
 
-// app.use(
-//     cors({
-//         origin: [
-//             "http://localhost:5173",
-//             process.env.FRONTEND_URL,
-//         ],
-//     })
-// );
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL,
-];
-
 app.use(
-  cors({
-    origin: (origin, cb) =>
-      !origin || allowedOrigins.includes(origin)
-        ? cb(null, true)
-        : cb(new Error("CORS blocked")),
-  })
+    cors({
+        origin: [
+            "http://localhost:5173",
+            process.env.FRONTEND_URL,
+        ],
+    })
 );
-
 
 app.use(express.json());
 
